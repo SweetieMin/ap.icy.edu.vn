6d4afdefd0ba16d59f795fb2e75cff9d9c149dc2
Nguyễn Khắc Huấn <smyth@Smyths-Mac-Mini.local>
2025-09-21 11:49:39 +0700

    production version 1.2

--- app/Http/Middleware/PreventBackHistory.php
+++ app/Http/Middleware/PreventBackHistory.php
@@ -16,8 +16,9 @@ class PreventBackHistory
     public function handle(Request $request, Closure $next): Response
     {
         $response = $next($request);
-        $response->headers->set('Cache-Control',' no-cache, no-store, max-age=0; must-revalidate');
-        $response->headers->set('Pragma','no-cache');
+
+        $response->headers->set('Cache-Control', 'no-cache, no-store, must-revalidate');
+        $response->headers->set('Pragma', 'no-cache'); // cho IE cũ
         $response->headers->set('Expires','Sun, 02 Jan 1990 00:00:00 GMT');
 
         return $response;
