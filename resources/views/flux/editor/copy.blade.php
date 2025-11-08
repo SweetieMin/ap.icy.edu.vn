fca105ba7fd788b8312f52c8564dba55be460303
SweetieMin <thomassmyth.isaac@gmail.com>
2025-11-08 14:10:26 +0700

    thêm editor

--- resources/views/flux/editor/copy.blade.php
+++ resources/views/flux/editor/copy.blade.php
@@ -1,6 +1,6 @@
 <flux:tooltip content="{{ __('Copy to clipboard') }}" class="contents">
     <flux:editor.button x-on:click="navigator.clipboard?.writeText($el.closest('[data-flux-editor]').value); $el.setAttribute('data-copied', ''); setTimeout(() => $el.removeAttribute('data-copied'), 2000)">
-        <flux:icon.clipboard variant="outline" class="[[data-copied]_&]:hidden size-5!" />
-        <flux:icon.clipboard-document-check variant="outline" class="hidden [[data-copied]_&]:block size-5!" />
+        <flux:icon.clipboard variant="outline" class="in-data-copied:hidden size-5!" />
+        <flux:icon.clipboard-document-check variant="outline" class="hidden in-data-copied:block size-5!" />
     </flux:editor.button>
 </flux:tooltip>
